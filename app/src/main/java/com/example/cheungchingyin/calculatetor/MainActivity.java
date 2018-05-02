6f36d58dea73a02a083d0a2119607d4cae5d38c1
CheungChingYin <CheungChingYin@outlook.com>
2018-05-02 15:21:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cheungchingyin/calculatetor/MainActivity.java
@@ -0,0 +1,255 @@
+package com.example.cheungchingyin.calculatetor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.cheungchingyin.util.Operation;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private TextView tvDisplay;
+    private Button btnClear;
+    private Button btnDel;
+    private Button btnMod;
+    private Button btnDivide;
+    private Button btn7;
+    private Button btn8;
+    private Button btn9;
+    private Button btnMultiply;
+    private Button btn4;
+    private Button btn5;
+    private Button btn6;
+    private Button btnMinus;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btnPlus;
+    private Button btn0;
+    private Button btnPoint;
+    private Button btnEquit;
+    private String display = "";
+    private double firstNum = 0.0;
+    private double secondNum = 0.0;
+    private double result = 0.0;
+    private boolean flag = false;
+    private String operation = null;
+    Operation op = new Operation();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.tv_display);
+        btnClear = findViewById(R.id.btn_clear);
+        btnDel = findViewById(R.id.btn_del);
+        btnMod = findViewById(R.id.btn_mod);
+        btnDivide = findViewById(R.id.btn_divide);
+        btn7 = findViewById(R.id.btn_7);
+        btn8 = findViewById(R.id.btn_8);
+        btn9 = findViewById(R.id.btn_9);
+        btnMultiply = findViewById(R.id.btn_multiply);
+        btn4 = findViewById(R.id.btn_4);
+        btn5 = findViewById(R.id.btn_5);
+        btn6 = findViewById(R.id.btn_6);
+        btnMinus = findViewById(R.id.btn_mius);
+        btn1 = findViewById(R.id.btn_1);
+        btn2 = findViewById(R.id.btn_2);
+        btn3 = findViewById(R.id.btn_3);
+        btnPlus = findViewById(R.id.btn_plus);
+        btn0 = findViewById(R.id.btn_0);
+        btnPoint = findViewById(R.id.btn_point);
+        btnEquit = findViewById(R.id.btn_equit);
+
+        btnClear.setOnClickListener(this);
+        btnDel.setOnClickListener(this);
+        btnMod.setOnClickListener(this);
+        btnDivide.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btnMultiply.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btnMinus.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btnPlus.setOnClickListener(this);
+        btn0.setOnClickListener(this);
+        btnPoint.setOnClickListener(this);
+        btnEquit.setOnClickListener(this);
+
+
+    }
+
+    public void onClick(View v) {
+
+        String str = null;
+        switch (v.getId()) {
+            case R.id.btn_clear:
+                display = "";
+                tvDisplay.setText(display);
+                firstNum = 0.0;
+                secondNum = 0.0;
+                result = 0.0;
+                flag = false;
+                break;
+
+            case R.id.btn_del:
+                display = display.substring(0, display.length() - 1);
+                tvDisplay.setText(display);
+                break;
+
+            case R.id.btn_mod:
+                if (flag) {
+                    break;
+                } else {
+                    str = tvDisplay.getText().toString();
+                    firstNum = Double.parseDouble(str);
+                    operation = "mod";
+                    display = "";
+                    tvDisplay.setText(display);
+                    break;
+                }
+
+            case R.id.btn_divide:
+                if (flag) {
+                    break;
+                } else {
+                    str = tvDisplay.getText().toString();
+                    firstNum = Double.parseDouble(str);
+                    operation = "divide";
+                    display = "";
+                    tvDisplay.setText(display);
+                    break;
+                }
+
+            case R.id.btn_7:
+                display += 7;
+                tvDisplay.setText(display);
+                break;
+
+            case R.id.btn_8:
+                display += 8;
+                tvDisplay.setText(display);
+                break;
+
+            case R.id.btn_9:
+                display += 9;
+                tvDisplay.setText(display);
+                break;
+
+            case R.id.btn_multiply:
+                if (flag) {
+                    break;
+                } else {
+                    str = tvDisplay.getText().toString();
+                    firstNum = Double.parseDouble(str);
+                    operation = "multiply";
+                    display = "";
+                    tvDisplay.setText(display);
+                    break;
+                }
+
+            case R.id.btn_4:
+                display += 4;
+                tvDisplay.setText(display);
+                break;
+
+            case R.id.btn_5:
+                display += 5;
+                tvDisplay.setText(display);
+                break;
+
+            case R.id.btn_6:
+                display += 6;
+                tvDisplay.setText(display);
+                break;
+
+            case R.id.btn_mius:
+                if (flag) {
+                    break;
+                } else {
+                    str = tvDisplay.getText().toString();
+                    firstNum = Double.parseDouble(str);
+                    operation = "minus";
+                    display = "";
+                    tvDisplay.setText(display);
+                    break;
+                }
+
+            case R.id.btn_1:
+                display += 1;
+                tvDisplay.setText(display);
+                break;
+
+            case R.id.btn_2:
+                display += 2;
+                tvDisplay.setText(display);
+                break;
+
+            case R.id.btn_3:
+                display += 3;
+                tvDisplay.setText(display);
+                break;
+
+            case R.id.btn_plus:
+                if (flag) {
+                    break;
+                } else {
+                    str = tvDisplay.getText().toString();
+                    firstNum = Double.parseDouble(str);
+                    operation = "plus";
+                    display = "";
+                    tvDisplay.setText(display);
+                    break;
+                }
+
+            case R.id.btn_0:
+                display += 0;
+                tvDisplay.setText(display);
+                break;
+
+            case R.id.btn_point:
+                display += ".";
+                tvDisplay.setText(display);
+                break;
+
+            case R.id.btn_equit:
+                str = tvDisplay.getText().toString();
+                secondNum = Double.parseDouble(str);
+                switch (operation) {
+                    case "plus":
+                        result = op.Plus(firstNum, secondNum);
+                        break;
+
+                    case "minus":
+                        result = op.Minus(firstNum, secondNum);
+                        break;
+
+                    case "multiply":
+                        result = op.Multiply(firstNum, secondNum);
+                        break;
+
+                    case "divide":
+                        result = op.Divide(firstNum, secondNum);
+                        break;
+
+                    case "mod":
+                        result = op.Mod(firstNum, secondNum);
+                        break;
+
+                }
+                display = result + "";
+                tvDisplay.setText(display);
+                flag = false;
+                break;
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/cheungchingyin/util/Operation.java
@@ -0,0 +1,23 @@
+package com.example.cheungchingyin.util;
+
+public class Operation {
+    public double Plus(double firstNum, double secondNum) {
+        return firstNum + secondNum;
+    }
+
+    public double Minus(double firstNum, double secondNum) {
+        return firstNum - secondNum;
+    }
+
+    public double Multiply(double firstNum, double secondNum) {
+        return firstNum * secondNum;
+    }
+
+    public double Divide(double firstNum, double secondNum) {
+        return firstNum / secondNum;
+    }
+
+    public double Mod(double firstNum, double secondNum) {
+        return firstNum % secondNum;
+    }
+}
